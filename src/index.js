579d9f1fadb332b26f85f80611a100fb6230990a
olipliche <olipliche@gmail.com>
2021-09-20 00:23:38 +0100

    update files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
- import _ from 'lodash';
- import './style.css';
+import './style.css';
 
- function component() {
-   const element = document.createElement('div');
+function component() {
+  const element = document.createElement('div');
+  element.classList.add('hello');
 
-  // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-   element.classList.add('hello');
+  return element;
+}
 
-   return element;
- }
-
- document.body.appendChild(component());
+document.body.appendChild(component());
